2fe19e25b14092b806957afd7d2d462eb665afde
Drake Ballew <drakeballew@gmail.com>
2018-10-06 11:28:16 -0700

    Add Add classes_and_objects_2

---
+++ exercises/classes_and_objects_2/exercise9.rb
@@ -0,0 +1,19 @@
+class Person
+  attr_writer :secret
+
+  def share_secret
+    secret
+  end
+
+  private
+
+  attr_reader :secret
+
+  def initialize
+    @secret = ''
+  end
+end
+
+person1 = Person.new
+person1.secret = 'Shh.. this is a secret!'
+puts person1.share_secret
